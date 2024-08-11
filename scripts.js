8bbbf0b2bae2cfb56e87a515512acf888e3e9480
mesguerra76 <mesua6976@gmail.com>
2024-08-11 18:49:45 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,71 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("mensaje").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      muñeco.src ="./img/encriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("mensaje").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      }
+  }
+  function limpiar() {
+    document.getElementById("texto").value = '';
+    document.getElementById("mensaje").value = '';
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    
+  }
+  
+  function copiar() {
+    var text = document.getElementById("mensaje").value;
+    copyToClipboard(text);
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+      .then(() => {          
+      })
+      .catch(err => {
+          });
+     }
+
+
